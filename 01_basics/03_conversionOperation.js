582aecee78c41d6640d0604c4d82df5dd27aecb6
wania <siddiqwania21@gmail.com>
2025-12-20 19:52:34 +0000

    commit lecture 7

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
  let isLoggedIn = "0"
 
  let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //  1 => true; 0 => false
 //  "" => false
@@ -24,8 +24,56 @@ let valueInNumber = Number(score)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(typeof someNumber);
+//console.log(typeof stringNumber);
+//console.log(typeof someNumber);
+
+//**********************OPERATIONS********************************** */
+
+let value = 3
+let negValue  = - value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "wania"
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+  
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 200
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  
