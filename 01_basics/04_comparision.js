0025603ddd68ccb58a689ec8dbc6ff30eb973472
wania <siddiqwania21@gmail.com>
2025-12-22 08:53:28 +0000

    change in location of file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); //comparision n equality check is diff
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
+
+
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,38 @@
+// primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 200.33
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+// const bigNumber = 23211231132322334432n
+
+
+
+// reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["wania","astra", "shaifu"];
+let myObj = {
+    name: "wania",
+    age: 101,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof null);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
