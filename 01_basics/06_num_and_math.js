c3fca9a605133d76d0b811b1ad76a14fb29b9eff
wania <siddiqwania21@gmail.com>
2025-12-23 09:27:51 +0000

    num and math commit

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 91.000002102
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++ Maths +++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.33));
+// console.log(Math.ceil(4.33));
+// console.log(Math.floor(4.33));
+// console.log(Math.min(4, 3, 55, 1));
+// console.log(Math.max(43, 543, 31, 999));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
