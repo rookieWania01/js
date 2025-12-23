1230eedfb7af13774e3c20ba1d579dd8403cca3d
wania <siddiqwania21@gmail.com>
2025-12-23 10:37:37 +0000

    date and time

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,36 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// let myCreatedDate = new Date(205, 0, 23)
+// let myCreatedDate = new Date(2025, 0, 23, 5, 4)
+// let myCreatedDate = new Date("2025-01-14") //in this format months start from 1
+let myCreatedDate = new Date("01-14-2025")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
+
+
+
+
+
+
+
+
