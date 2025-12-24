a37da9fc4c7dedd318b7ae8de6c5527325ffffa9
wania <siddiqwania21@gmail.com>
2025-12-24 21:14:42 +0000

    array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4 ,5 ]
+const myHeroes = ["me", "myself"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[3]);
+console.log(myArr.length);
+
+// array methods
+
+
+// myArr.push(21) // to add new elemnt in array
+// myArr.pop()    // removes the last elemnt in array
+// console.log(myArr);
+// myArr.unshift(9)  // array ke starting  mey value push
+// myArr.shift()    // removes first elemnt from array
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(9));
+
+const newArr = myArr.join()
+//console.log(newArr);
+
+// slice,spice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+
+console.log(myn2);
+
+
+
+
+
+
+
