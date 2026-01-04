0b81e2ce1e9043c8dfa4ab589a36c319d035a1fb
wania <siddiqwania21@gmail.com>
2026-01-04 09:43:09 +0000

    arrows

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -13,10 +13,37 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
 
 // function chai(){ 
-    // console.log(this)
+//     console.log(this)
 // }
 
 // chai()
+
+// const chai = function (){
+//     let username = "wania"
+//     console.log(this.username);
+// }
+
+// chai()    output is undefined in this case
+
+const chai = () => {
+    let username = "wania"
+    console.log(this);    
+}
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)  //{} ni hai so dont use return
+const addTwo = (num1, num2) => ({username: "wania"}) //object ko return krne ke liye gotta wrap it in parenthesis
+
+
+console.log(addTwo(19, 16));
+
+ 
