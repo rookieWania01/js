666871cf89227c5e7e811b7c536be2fcb8065010
wania <siddiqwania21@gmail.com>
2026-01-09 17:10:25 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,25 @@
+// for 
+let n = 20
+// for (let i = 0;i <= n;i++) {
+   //  console.log(i );
+   //  if(i ==5) console.log('5 is best no');
+//  }
+//  for(let i = 0; i<=5;i++){
+//     for(let j = 0; j<=5;j++)
+//     console.log(i + "*" + j +'=' + i*j);    
+//  }
+
+ let myArr = ["wanii", "astra", "saifu"]
+ for(let i =0;i < myArr.length; i++){
+    console.log(myArr[i]);
+ }
+
+ // break and continue
+
+ for(let i = 1; i<10;i++){
+   if(i%2===0) console.log(i);
+   else continue;
+   if(i==-9) break;
+   
+ }
+ 
