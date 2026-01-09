d006b26627a402dbe0eba6f08be5ba143a343804
wania <siddiqwania21@gmail.com>
2026-01-09 19:52:43 +0000

    iterations

---
+++ 05_iterations/three.js
@@ -0,0 +1,22 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);  
+}
+
+const greetings = " hello world"
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`);
+}
+
+// Maps
+
+const map = new map()
+map.set('IN',"india")
+map.set('USA', "united states of america")
+map.set('Fr',"france")
