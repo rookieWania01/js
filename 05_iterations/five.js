847b9cf05bd52860f2c54745dd5e36ca698ad8b2
wania <siddiqwania21@gmail.com>
2026-01-10 12:53:27 +0000

    loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,35 @@
+const coding = ["js", "ruby", "cpp", "python", "rust"]
+
+// coding.forEach( function (item)  {
+//     console.log(item);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach( (item, index, arr) {
+//     console.log(item, index,arr);
+// } )
+
+const mycoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "java",
+    },
+    {
+    },
+    {
+    },
+]
+
+mycoding.forEach( (item) => {
+    console.log(item.languageName);    
+})
