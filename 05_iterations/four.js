6da88c3ecddffcbcda44cc832bfffb4788e95d42
wania <siddiqwania21@gmail.com>
2026-01-10 09:13:43 +0000

    loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,25 @@
+// const myobj = {
+//     js: 'javascript',
+//     rb: 'ruby',
+//     cpp: 'c++',
+//     swift: 'swift by apple',
+// }
+
+// for (const key in myobj) {
+//   console.log(`${key} shortcut is for ${myobj[key]} `);
+// }
+
+// const myarr = ["js", "cpp", "c", "py"]
+
+// for (const key in myarr) {
+//     console.log(myarr[key]);
+// }
+
+// const map = new Map()
+// map.set('IN', "india")
+// map.set('jp', "japan")
+// map.set('Fr', "france")
+
+// for (const key in object) {
+//    console.log(key);
+// }
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -3,20 +3,38 @@
 // ["", "", ""]
 // [{}, {}, {}]
 
-const arr = [1, 2, 3, 4, 5]
+// const arr = [1, 2, 3, 4, 5]
 
-for (const num of arr) {
-    console.log(num);  
-}
+// for (const num of arr) {
+//     console.log(num);  
+// }
 
-const greetings = " hello world"
-for (const greet of greetings) {
-    console.log(`each char is ${greet}`);
-}
+// const greetings = "oyasumi dayo"
+
+
+// for (const greet of greetings) {
+//     console.log(greet);
+// }
 
 // Maps
 
-const map = new map()
-map.set('IN',"india")
-map.set('USA', "united states of america")
-map.set('Fr',"france")
+const map = new Map()
+map.set('IN', "india")
+map.set('jp', "japan")
+map.set('Fr', "france")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+// const myobj = {
+//     'game1': 'nfs',
+//     'game2': 'coc',
+// }
+
+// for (const [key, value] of myobj) {
+//     console.log(key,':-', value);
+    
+// }
